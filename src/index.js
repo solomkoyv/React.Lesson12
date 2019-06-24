64f7e96986e99e6b03af04c66236c2f7045930b3
Drive-Mac <Drive-Mac@Drives-MacBook.local>
2019-06-24 13:04:31 +0300

    ДЗ урок 12 redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "../src/index.min.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from "redux";
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case "INC":
+      return state + 1;
+    case "DEC":
+      return state - 1;
+    case "RND":
+      return state + action.value;
+    default:
+      return state;
+  }
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const inc = () => ({ type: "INC" });
+const dec = () => {
+  return { type: "DEC" };
+};
+const rnd = value => {
+  return { type: "RND", value };
+};
+
+const store = createStore(reducer);
+
+document.getElementById("inc").addEventListener("click", () => {
+  store.dispatch(inc());
+});
+document.getElementById("dec").addEventListener("click", () => {
+  store.dispatch(dec());
+});
+document.getElementById("rnd").addEventListener("click", () => {
+  const value = Math.floor(Math.random() * 10);
+  store.dispatch(rnd(value));
+});
+
+const update = () => {
+  document.getElementById("counter").textContent = store.getState();
+};
+
+store.subscribe(update);
+
+// store.dispatch({ type: "INC" });
+// store.dispatch({ type: "INC" });
+// store.dispatch({ type: "INC" });
